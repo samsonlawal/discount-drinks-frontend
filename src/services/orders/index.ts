aaf769acaf04686f3c60b699ad40402d03bd7c28
deji <lawal4211@gmail.com>
2026-04-05 16:50:32 +0100

    fix: Auth bearer class order error

--- src/services/orders/index.ts
+++ src/services/orders/index.ts
@@ -3,43 +3,38 @@ import env from "@/config/env";
 import { OrderInterface, TCreateOrderService } from "./types";
 import { store } from "@/redux/store";
 
-
-    const token = store.getState().auth.accessToken;
-
 class Service implements OrderInterface {
+  private getAuthHeader() {
+    const token = store.getState().auth.accessToken;
+    return token ? { Authorization: `Bearer ${token}` } : {};
+  }
+
   createOrder({ payload }: TCreateOrderService) {
     return axios.post(env.api.orders, payload, {
-
+      headers: this.getAuthHeader(),
       withCredentials: true,
-      headers: { Authorization: `Bearer ${token}` },
-
     });
   }
 
   createCheckout(payload: any) {
     return axios.post(env.api.checkout, payload, {
-
+      headers: this.getAuthHeader(),
       withCredentials: true,
-      headers: { Authorization: `Bearer ${token}` },
-
-    })
+    });
   }
 
   getUserOrders(userId: string, query?: any) {
     return axios.get(`${env.api.orders}/user/${userId}`, {
-
       params: query,
+      headers: this.getAuthHeader(),
       withCredentials: true,
-      headers: { Authorization: `Bearer ${token}` },
-
     });
   }
 
   getOrderById(orderId: string) {
     return axios.get(`${env.api.orders}/${orderId}`, {
+      headers: this.getAuthHeader(),
       withCredentials: true,
-      headers: { Authorization: `Bearer ${token}` },
-
     });
   }
 }
